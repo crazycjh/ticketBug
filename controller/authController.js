9aa2cf20b42338ac917abda24e117fbae7fa6a91
ChenHao Chen <crazycjh@gmail.com>
2024-01-31 00:01:55 +0800

    google Oauth 後端功能

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,14 @@
 // const crypto = require('crypto');
 // const { promisify } = require('util');
-// const jwt = require('jsonwebtoken');
+
+const { UserInfo } = require('../models/userModel')
+
 const { OAuth2Client } = require('google-auth-library');
 const { google } = require('googleapis');
+const jwt = require('jsonwebtoken');
+
+// 設定JWT
+const secretKey = process.env.JWT_SECRETKEY
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
@@ -10,7 +16,36 @@ const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI
 
 const oAuth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI);
 
+// 建立JWT
+const signToken = ( email ) => {
+    return jwt.sign(
+        { email }, 
+        secretKey, 
+        {
+            expiresIn:process.env.JWT_EXPIRES_IN
+        }
+    )
+} 
 
+const createSendToken = ( email, statusCode, res ) => {
+    const token = signToken( email );
+    
+    // 設定回傳Cookie的options
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly:true
+    }
+
+    // 產品模式改成HTTPS
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
+    
+    res.status(statusCode).redirect('http://localhost:5173/membercenter');
+
+}
 
 exports.googleAuth = (req, res) => {
     const scopes = [
@@ -25,8 +60,9 @@ exports.googleAuth = (req, res) => {
 }
 
 exports.googleAuthCallback = async(req, res) => {
+    let statusCode;
     const code = req.query.code;
-
+    
     // get access token
     const {tokens} = await oAuth2Client.getToken(code);
     oAuth2Client.setCredentials(tokens);
@@ -40,10 +76,38 @@ exports.googleAuthCallback = async(req, res) => {
     });
 
     // 使用者相關帳號建立或是讀取資料
+    // 確認是否已經存在
 
     
-    console.log(me.data);
 
-    res.redirect('http://localhost:5173');
+    // 存在/不存在都回傳相關帳號資料
+    const result = await findUser(me.data.emailAddresses[0].value)
+    
+    //不存在 -> 建立帳號
+    if(!result) {
+        createAccountByGoogleLogin(me.data.emailAddresses[0].value)
+        // 創建新的
+        statusCode = 201;
+        
+    }else {
+        statusCode = 200;
+    }
+    
+    createSendToken(me.data.emailAddresses[0].value, statusCode, res);
+    // console.log(me.data.emailAddresses[0].value);
+
+    
 
 }
+
+async function findUser(email) {
+    const user = await UserInfo.findOne({where: {email: email}});
+    return user;
+}
+// const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
+function createAccountByGoogleLogin(email) {
+    // 建立base on Google登入的帳號，密碼會是空的，下次使用者可以透過忘記密碼幫忙建立一個，再由使用者修改，或是進到設定設定密碼
+    const result = UserInfo.create({ email:email, google:true})
+}
+
+// 建立使用者
---
+++ models/userModel.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utility/db/databaseConnect')
+
+const UserInfo = sequelize.define('UserInfo', {
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        isEmail: true,
+        primaryKey: true,
+    },
+    password:DataTypes.STRING,
+    passwordConfirm:DataTypes.STRING,
+    passwordChangedAt:DataTypes.DATE,
+    passwordResetToken:DataTypes.STRING,
+    passwordResetExpires:DataTypes.DATE,
+    google: DataTypes.BOOLEAN,
+    
+},
+{
+    hooks: {
+        beforeCreate: (userInfo, options) => {
+            // 這兩者都可以存取要寫入的那些資料，也可以去修改它做一些操作
+            console.log(userInfo.dataValues.email); 
+            console.log(userInfo.email); 
+        },
+        afterCreate: (userInfo, options) => {
+            // 建立後的邏輯
+        }
+    },
+    tableName: 'UserInfo',
+    timestamps: false
+    // createdAt: 'creationDate',
+    
+},
+{
+    
+}
+
+);
+
+UserInfo.sync({ alter: true });
+// 同步模型到数据库
+
+
+
+module.exports = { UserInfo };
